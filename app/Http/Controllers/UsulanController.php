592ff311027b9a6f60ee7ac3ec35822d77d859cf
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-06 14:10:57 +0700

    Create usulan controller,model,seeder,migration

---
+++ app/Http/Controllers/UsulanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usulan;
+use Illuminate\Http\Request;
+
+class UsulanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Usulan  $usulan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Usulan $usulan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Usulan  $usulan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Usulan $usulan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Usulan  $usulan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usulan $usulan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Usulan  $usulan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usulan $usulan)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_12_06_043940_create_usulans_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usulans', function (Blueprint $table) {
+            $table->id();
+            $$table->string('No');
+            $table->date('Tgl_Usul');
+            $$table->string('Pengusul');
+            $table->string('Profil');
+            $table->string('Urusan');
+            $table->text('Usulan');
+            $table->text('Permasalahan');
+            $table->string('Alamat');
+            $table->string('Kecamatan');
+            $table->string('Kelurahan');
+            $table->string('Usul_Ke');
+            $table->string('SKPD_Tujuan_Awal');
+            $table->string('SKPD_Tujuan_Akhir');
+            $table->string('Rekomendasi_Bappeda_Mitra_OPD');
+            $table->string('Koefisien');
+            $table->string('Anggaran');
+            $table->string('Kategori_Usulan');
+            $table->string('Koefisien_1');
+            $table->string('Rekomendasi_Kelurahan_Desa');
+            $table->string('Rekomendasi_Kecamatan');
+            $table->string('Koefisien_2');
+            $table->string('Anggaran_1');
+            $table->string('Rekomendasi_SKPD');
+            $table->string('Koefisien_3');
+            $table->string('Anggaran_2');
+            $table->string('Rekomendasi_Bappeda');
+            $table->string('Koefisien_4');
+            $table->string('Anggaran_3');
+            $table->string('Status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usulans');
+    }
+};
