f35f4bb486e822fe5dce57fbae9c7d0f0df78bad
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-07 21:04:59 +0700

    Fxi usulan table

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -76,6 +76,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
         <!-- AdminLTE App -->
         <script src="dist/js/adminlte.min.js"></script>
+        @yield('page_specific_script')
 </body>
 
 </html>
--- resources/views/usulan_fisik.blade.php
+++ resources/views/usulan_fisik.blade.php
@@ -14,32 +14,23 @@
                       <table id="example1" class="table table-bordered table-striped">
                           <thead>
                               <tr>
-                                  <th>Rendering engine</th>
-                                  <th>Browser</th>
-                                  <th>Platform(s)</th>
-                                  <th>Engine version</th>
-                                  <th>CSS grade</th>
+                                  <th>No</th>
+                                  <th>Pengusul</th>
+                                  <th>Profil</th>
+                                  <th>Urusan</th>
+                                  <th>Usulan</th>
                               </tr>
                           </thead>
                           <tbody>
+                              @foreach ($collection as $item)
                               <tr>
-                                  <td>Trident</td>
-                                  <td>Internet
-                                      Explorer 4.0
-                                  </td>
-                                  <td>Win 95+</td>
-                                  <td> 4</td>
-                                  <td>X</td>
+                                  <td>{{ $item->No }}</td>
+                                  <td>{{ $item->Pengusul }}</td>
+                                  <td>{{ $item->Profil }}</td>
+                                  <td>{{ $item->Urusan }}</td>
+                                  <td>{{ $item->Usulan }}</td>
                               </tr>
-                              <tr>
-                                  <td>Trident</td>
-                                  <td>Internet
-                                      Explorer 5.0
-                                  </td>
-                                  <td>Win 95+</td>
-                                  <td>5</td>
-                                  <td>C</td>
-                              </tr>
+                              @endforeach
                           </tbody>
                       </table>
                   </div>
@@ -54,4 +45,38 @@
   <!-- /.container-fluid -->
 </section>
 <!-- /.content -->
+@endsection
+
+@section('page_specific_script')
+<!-- DataTables  & Plugins -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../../plugins/jszip/jszip.min.js"></script>
+<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- Page specific script -->
+<script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
+    });
+  </script>
 @endsection
