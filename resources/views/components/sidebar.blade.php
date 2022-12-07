031438e6cff2c64807ee84c8b7e6b55fff429d21
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-07 14:20:44 +0700

    Add sidebar active class

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -39,7 +39,7 @@
                 <!-- Add icons to the links using the .nav-icon class
        with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="/" class="nav-link">
+                    <a href="/" class="nav-link {{ Request::is('/') ? 'active' : '' }}">
                         <i class="nav-icon fas fa-th"></i>
                         <p>
                             Dashboard
@@ -47,7 +47,7 @@
                     </a>
                 </li>
                 <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
+                    <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Usulan
@@ -56,18 +56,26 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="/usulan-fisik" class="nav-link active">
+                            <a href="/usulan-fisik" class="nav-link {{ Request::is('usulan-fisik') ? 'active' : '' }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Usulan Fisik</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="/usulan-non-fisik" class="nav-link">
+                            <a href="/usulan-non-fisik" class="nav-link {{ Request::is('usulan-non-fisik') ? 'active' : '' }}">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Usulan Non-Fisik</p>
                             </a>
                         </li>
                     </ul>
+                </li>
+                <li class="nav-item">
+                    <a href="history-usulan" class="nav-link {{ Request::is('history-usulan') ? 'active' : '' }}">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            History Usulan
+                        </p>
+                    </a>
                 </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
