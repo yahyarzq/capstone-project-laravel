54767a9c9811902be35ba420af8cef6bba3d7e0c
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-07 14:19:38 +0700

    Add new views

---
+++ resources/views/history_usulan.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts/main')
+@section('container')
+    <!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+      <div >
+          <div >
+              <div class="card">
+                  <div class="card-header">
+                      <h3 class="card-title">DataTable with default features</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                      <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Pengusul</th>
+                                <th>Profil</th>
+                                <th>Urusan</th>
+                                <th>Usulan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($collection as $item)
+                            <tr>
+                                <td>{{ $item->No }}</td>
+                                <td>{{ $item->Pengusul }}</td>
+                                <td>{{ $item->Profil }}</td>
+                                <td>{{ $item->Urusan }}</td>
+                                <td>{{ $item->Usulan }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+                  </div>
+                  <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+          </div>
+          <!-- /.col -->
+      </div>
+      <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+@endsection
---
+++ resources/views/usulan_non_fisik.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts/main')
+@section('container')
+    <!-- Main content -->
+<section class="content">
+  <div class="container-fluid">
+      <div >
+          <div >
+              <div class="card">
+                  <div class="card-header">
+                      <h3 class="card-title">DataTable with default features</h3>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body">
+                      <table id="example1" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Pengusul</th>
+                                <th>Profil</th>
+                                <th>Urusan</th>
+                                <th>Usulan</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($collection as $item)
+                            <tr>
+                                <td>{{ $item->No }}</td>
+                                <td>{{ $item->Pengusul }}</td>
+                                <td>{{ $item->Profil }}</td>
+                                <td>{{ $item->Urusan }}</td>
+                                <td>{{ $item->Usulan }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+                  </div>
+                  <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+          </div>
+          <!-- /.col -->
+      </div>
+      <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
+<!-- /.content -->
+@endsection
+
+@section('page_specific_script')
+<!-- DataTables  & Plugins -->
+<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="../../plugins/jszip/jszip.min.js"></script>
+<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
+<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- Page specific script -->
+<script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
+    });
+  </script>
+@endsection
