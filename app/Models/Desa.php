69597fcbfb251482b2602d37cfbea39f16de170e
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-09 08:47:31 +0700

    Create a one to many relationship, between the kecamatan and desa tables

--- app/Models/Desa.php
+++ app/Models/Desa.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Desa extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function kecamatan(){
+        return $this->belongsTo(Kecamatan::class);
+    }
 }
