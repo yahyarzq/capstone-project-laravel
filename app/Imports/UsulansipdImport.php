0676b71e9d2747036fdece3424ec7d4d87c135fd
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-09 22:22:35 +0700

    Add usulansipd import model

--- app/Imports/UsulansipdImport.php
+++ app/Imports/UsulansipdImport.php
@@ -15,7 +15,23 @@ class UsulansipdImport implements ToModel
     public function model(array $row)
     {
         return new Usulansipd([
-            //
+            'No' => $row[0],
+            'Tgl_Usul' => date('Y-m-d', strtotime($row[1])),
+            'Tgl_Pengajuan' => date('Y-m-d', strtotime($row[2])),
+            'Pengusul' => $row[3],
+            'Profil' => $row[4],
+            'Permasalahan' => $row[5],
+            'Usulan' => $row[6],
+            'Urusan' => $row[7],
+            'Alamat' => $row[8],
+            'SKPD_Tujuan_Awal' => $row[9],
+            'SKPD_Tujuan_Akhir' => $row[10],
+            'Rekomendasi_Bappeda_Mitra_OPD' => $row[11],
+            'Kategori_Usulan' => $row[12],
+            'Koefisien' => $row[13],
+            'Rekomendasi_Kelurahan_Desa' => $row[13],
+            'Rekomendasi_Kecamatan' => $row[14],
+            'Rekomendasi_SKPD' => $row[15],
         ]);
     }
 }
