816f300efa852fc18d44a2396ad578d5ed7be263
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-09 09:54:36 +0700

    Create table for uncleaned data, include controller, model,seeder, factorie,migration.

---
+++ app/Http/Controllers/UsulansipdController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usulansipd;
+use Illuminate\Http\Request;
+
+class UsulansipdController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Usulansipd  $usulansipd
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Usulansipd $usulansipd)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Usulansipd  $usulansipd
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Usulansipd $usulansipd)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Usulansipd  $usulansipd
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Usulansipd $usulansipd)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Usulansipd  $usulansipd
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Usulansipd $usulansipd)
+    {
+        //
+    }
+}
