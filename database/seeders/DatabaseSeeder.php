d30491b39f9aa75fa03fa9d5239ae389aa98afff
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-10 20:49:19 +0700

    Add new seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call([
-            UsulanSeeder::class
+            UsulanSeeder::class,
+            UsulansipdSeeder::class
         ]);
     }
 }
