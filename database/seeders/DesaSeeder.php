998574e1283e2150531a9856dac667a9a60d3be3
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-11 12:22:12 +0700

    Add list of desa and kecamatan.

--- database/seeders/DesaSeeder.php
+++ database/seeders/DesaSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Desa;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,339 @@ class DesaSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $desas = [ [
+            "id" => 3507010001,
+            "Kecamatan_id" => 3507010,
+            "nama" => "SUMBEROTO",
+
+        ],
+        [
+            "id" => 3507010002,
+            "Kecamatan_id" => 3507010,
+            "nama" => "PURWOREJO",
+
+        ],
+        [
+            "id" => 3507010003,
+            "Kecamatan_id" => 3507010,
+            "nama" => "MENTARAMAN",
+
+        ],
+        [
+            "id" => 3507010004,
+            "Kecamatan_id" => 3507010,
+            "nama" => "DONOMULYO",
+
+        ],
+        [
+            "id" => 3507010005,
+            "Kecamatan_id" => 3507010,
+            "nama" => "TEMPURSARI",
+
+        ],
+        [
+            "id" => 3507010006,
+            "Kecamatan_id" => 3507010,
+            "nama" => "TLOGOSARI",
+
+        ],
+        [
+            "id" => 3507010007,
+            "Kecamatan_id" => 3507010,
+            "nama" => "KEDUNGSALAM",
+
+        ],
+        [
+            "id" => 3507010008,
+            "Kecamatan_id" => 3507010,
+            "nama" => "BANJARJO",
+
+        ],
+        [
+            "id" => 3507010009,
+            "Kecamatan_id" => 3507010,
+            "nama" => "TULUNGREJO",
+
+        ],
+        [
+            "id" => 3507010010,
+            "Kecamatan_id" => 3507010,
+            "nama" => "PURWODADI",
+
+        ],
+        [
+            "id" => 3507020001,
+            "Kecamatan_id" => 3507020,
+            "nama" => "ARJOSARI",
+
+        ],
+        [
+            "id" => 3507020002,
+            "Kecamatan_id" => 3507020,
+            "nama" => "TUMPAKREJO",
+
+        ],
+        [
+            "id" => 3507020003,
+            "Kecamatan_id" => 3507020,
+            "nama" => "KALIASRI",
+
+        ],
+        [
+            "id" => 3507020004,
+            "Kecamatan_id" => 3507020,
+            "nama" => "PUTUKREJO",
+
+        ],
+        [
+            "id" => 3507020006,
+            "Kecamatan_id" => 3507020,
+            "nama" => "KALIPARE",
+
+        ],
+        [
+            "id" => 3507020007,
+            "Kecamatan_id" => 3507020,
+            "nama" => "SUKOWILANGUN",
+
+        ],
+        [
+            "id" => 3507020008,
+            "Kecamatan_id" => 3507020,
+            "nama" => "ARJOWILANGUN",
+
+        ],
+        [
+            "id" => 3507020009,
+            "Kecamatan_id" => 3507020,
+            "nama" => "KALIREJO",
+
+        ],
+        [
+            "id" => 3507030001,
+            "Kecamatan_id" => 3507030,
+            "nama" => "SUMBERMANJING KULON",
+
+        ],
+        [
+            "id" => 3507030002,
+            "Kecamatan_id" => 3507030,
+            "nama" => "PANDANREJO",
+
+        ],
+        [
+            "id" => 3507030003,
+            "Kecamatan_id" => 3507030,
+            "nama" => "SUMBERKERTO",
+
+        ],
+        [
+            "id" => 3507030004,
+            "Kecamatan_id" => 3507030,
+            "nama" => "SEMPOL",
+
+        ],
+        [
+            "id" => 3507030005,
+            "Kecamatan_id" => 3507030,
+            "nama" => "PAGAK",
+
+        ],
+        [
+            "id" => 3507030006,
+            "Kecamatan_id" => 3507030,
+            "nama" => "SUMBERREJO",
+
+        ],
+        [
+            "id" => 3507030007,
+            "Kecamatan_id" => 3507030,
+            "nama" => "GAMPINGAN",
+
+        ],
+        [
+            "id" => 3507030008,
+            "Kecamatan_id" => 3507030,
+            "nama" => "TLOGOREJO",
+
+        ],
+        [
+            "id" => 3507040001,
+            "Kecamatan_id" => 3507040,
+            "nama" => "BANDUNGREJO",
+
+        ],
+        [
+            "id" => 3507040002,
+            "Kecamatan_id" => 3507040,
+            "nama" => "SUMBERBENING",
+
+        ],
+        [
+            "id" => 3507040003,
+            "Kecamatan_id" => 3507040,
+            "nama" => "SRIGONCO",
+
+        ],
+        [
+            "id" => 3507040004,
+            "Kecamatan_id" => 3507040,
+            "nama" => "WONOREJO",
+
+        ],
+        [
+            "id" => 3507040005,
+            "Kecamatan_id" => 3507040,
+            "nama" => "BANTUR",
+
+        ],
+        [
+            "id" => 3507040006,
+            "Kecamatan_id" => 3507040,
+            "nama" => "PRINGGODANI",
+
+        ],
+        [
+            "id" => 3507040007,
+            "Kecamatan_id" => 3507040,
+            "nama" => "REJOSARI",
+
+        ],
+        [
+            "id" => 3507040008,
+            "Kecamatan_id" => 3507040,
+            "nama" => "WONOKERTO",
+
+        ],
+        [
+            "id" => 3507040009,
+            "Kecamatan_id" => 3507040,
+            "nama" => "REJOYOSO",
+
+        ],
+        [
+            "id" => 3507040010,
+            "Kecamatan_id" => 3507040,
+            "nama" => "KARANGSARI",
+
+        ],
+        [
+            "id" => 3507050001,
+            "Kecamatan_id" => 3507050,
+            "nama" => "TUMPAKREJO",
+
+        ],
+        [
+            "id" => 3507050002,
+            "Kecamatan_id" => 3507050,
+            "nama" => "SINDUREJO",
+
+        ],
+        [
+            "id" => 3507050003,
+            "Kecamatan_id" => 3507050,
+            "nama" => "GAJAHREJO",
+
+        ],
+        [
+            "id" => 3507050004,
+            "Kecamatan_id" => 3507050,
+            "nama" => "SIDODADI",
+
+        ],
+        [
+            "id" => 3507050005,
+            "Kecamatan_id" => 3507050,
+            "nama" => "GEDANGAN",
+
+        ],
+        [
+            "id" => 3507050006,
+            "Kecamatan_id" => 3507050,
+            "nama" => "SEGARAN",
+
+        ],
+        [
+            "id" => 3507050007,
+            "Kecamatan_id" => 3507050,
+            "nama" => "SUMBEREJO",
+
+        ],
+        [
+            "id" => 3507050008,
+            "Kecamatan_id" => 3507050,
+            "nama" => "GIRIMULYO",
+
+        ],
+        [
+            "id" => 3507060001,
+            "Kecamatan_id" => 3507060,
+            "nama" => "SITIARJO",
+
+        ],
+        [
+            "id" => 3507060002,
+            "Kecamatan_id" => 3507060,
+            "nama" => "TAMBAKREJO",
+
+        ],
+        [
+            "id" => 3507060003,
+            "Kecamatan_id" => 3507060,
+            "nama" => "KEDUNGBANTENG",
+
+        ],
+        [
+            "id" => 3507060004,
+            "Kecamatan_id" => 3507060,
+            "nama" => "TAMBAKASRI",
+
+        ],
+        [
+            "id" => 3507060005,
+            "Kecamatan_id" => 3507060,
+            "nama" => "TEGALREJO",
+
+        ],
+        [
+            "id" => 3507060006,
+            "Kecamatan_id" => 3507060,
+            "nama" => "RINGINKEMBAR",
+
+        ],
+        [
+            "id" => 3507060007,
+            "Kecamatan_id" => 3507060,
+            "nama" => "SUMBERAGUNG",
+
+        ],
+        [
+            "id" => 3507060008,
+            "Kecamatan_id" => 3507060,
+            "nama" => "HARJOKUNCARAN",
+
+        ],
+        [
+            "id" => 3507060009,
+            "Kecamatan_id" => 3507060,
+            "nama" => "ARGOTIRTO",
+
+        ],
+        [
+            "id" => 3507060010,
+            "Kecamatan_id" => 3507060,
+            "nama" => "RINGINSARI",
+
+        ],
+        [
+            "id" => 3507060012,
+            "Kecamatan_id" => 3507060,
+            "nama" => "SUMBERMANJING WETAN",
+        ]
+    ];
+        foreach ($desas as $desa) {
+            Desa::create($desa);
+        }
+        
     }
 }
--- database/seeders/KecamatanSeeder.php
+++ database/seeders/KecamatanSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Kecamatan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,208 @@ class KecamatanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $kecamatans = [
+            [
+                "id" => 3507010,
+                "nama" => "DONOMULYO",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507020,
+                "nama" => "KALIPARE",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507030,
+                "nama" => "PAGAK",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507040,
+                "nama" => "BANTUR",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507050,
+                "nama" => "GEDANGAN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507060,
+                "nama" => "SUMBERMANJING",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507070,
+                "nama" => "DAMPIT",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507080,
+                "nama" => "TIRTO YUDO",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507090,
+                "nama" => "AMPELGADING",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507100,
+                "nama" => "PONCOKUSUMO",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507110,
+                "nama" => "WAJAK",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507120,
+                "nama" => "TUREN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507130,
+                "nama" => "BULULAWANG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507140,
+                "nama" => "GONDANGLEGI",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507150,
+                "nama" => "PAGELARAN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507160,
+                "nama" => "KEPANJEN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507170,
+                "nama" => "SUMBER PUCUNG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507180,
+                "nama" => "KROMENGAN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507190,
+                "nama" => "NGAJUM",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507200,
+                "nama" => "WONOSARI",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507210,
+                "nama" => "WAGIR",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507220,
+                "nama" => "PAKISAJI",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507230,
+                "nama" => "TAJINAN",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507240,
+                "nama" => "TUMPANG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507250,
+                "nama" => "PAKIS",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507260,
+                "nama" => "JABUNG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507270,
+                "nama" => "LAWANG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507280,
+                "nama" => "SINGOSARI",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507290,
+                "nama" => "KARANGPLOSO",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507300,
+                "nama" => "DAU",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507310,
+                "nama" => "PUJON",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507320,
+                "nama" => "NGANTANG",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ],
+            [
+                "id" => 3507330,
+                "nama" => "KASEMBON",
+                "created_at" => NULL,
+                "updated_at" => NULL,
+            ]];
+            foreach ($kecamatans as $kecamatan ) {
+                Kecamatan::create($kecamatan);
+            }
+        
     }
 }
