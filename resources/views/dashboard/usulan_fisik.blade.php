d0aa424a3aa53aa363398a789caf8ec1213f8dad
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-13 20:49:47 +0700

    Minor changes - Move back content header - fix blade layout - move all scripts to main layouts - fix table header & body - add table hover & text nowrap - remove table footer

--- resources/views/dashboard/usulan_fisik.blade.php
+++ resources/views/dashboard/usulan_fisik.blade.php
@@ -1,82 +1,154 @@
-@extends('layouts/main')
+@extends('dashboard/layouts/main')
 @section('container')
-    <!-- Main content -->
-<section class="content">
-  <div class="container-fluid">
-      <div >
-          <div >
-              <div class="card">
-                  <div class="card-header">
-                      <h3 class="card-title">DataTable with default features</h3>
-                  </div>
-                  <!-- /.card-header -->
-                  <div class="card-body">
-                      <table id="example1" class="table table-bordered table-striped">
-                          <thead>
-                              <tr>
-                                  <th>No</th>
-                                  <th>Pengusul</th>
-                                  <th>Profil</th>
-                                  <th>Urusan</th>
-                                  <th>Usulan</th>
-                              </tr>
-                          </thead>
-                          <tbody>
-                              @foreach ($collection as $item)
-                              <tr>
-                                  <td>{{ $item->No }}</td>
-                                  <td>{{ $item->Pengusul }}</td>
-                                  <td>{{ $item->Profil }}</td>
-                                  <td>{{ $item->Urusan }}</td>
-                                  <td>{{ $item->Usulan }}</td>
-                              </tr>
-                              @endforeach
-                          </tbody>
-                      </table>
-                  </div>
-                  <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
-          </div>
-          <!-- /.col -->
-      </div>
-      <!-- /.row -->
-  </div>
-  <!-- /.container-fluid -->
-</section>
-<!-- /.content -->
-@endsection
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>DataTables</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">DataTables</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
-@section('page_specific_script')
-<!-- DataTables  & Plugins -->
-<script src="../../plugins/datatables/jquery.dataTables.min.js"></script>
-<script src="../../plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-<script src="../../plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
-<script src="../../plugins/jszip/jszip.min.js"></script>
-<script src="../../plugins/pdfmake/pdfmake.min.js"></script>
-<script src="../../plugins/pdfmake/vfs_fonts.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.html5.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.print.min.js"></script>
-<script src="../../plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
-<!-- Page specific script -->
-<script>
-    $(function () {
-      $("#example1").DataTable({
-        "responsive": true, "lengthChange": false, "autoWidth": false,
-        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-      $('#example2').DataTable({
-        "paging": true,
-        "lengthChange": false,
-        "searching": false,
-        "ordering": true,
-        "info": true,
-        "autoWidth": false,
-        "responsive": true,
-      });
-    });
-  </script>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">DataTable with default features</h3>
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body ">
+                            <div id="example1_head" class="mb-3"></div>
+                            {{--add table-responsive to maka table responsive horizontal--}}
+                            <table id="example1" class="table table-bordered table-hover table-striped text-nowrap" >
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Tanggal Usul</th>
+                                        <th>Pengusul</th>
+                                        <th>Profil</th>
+                                        <th>Urusan</th>
+                                        <th>Usulan</th>
+                                        <th>Permasalahan</th>
+                                        <th>Alamat</th>
+                                        <th>Desa</th>
+                                        <th>Kecamatan</th>
+                                        <th>Usul_Ke</th>
+                                        <th>SKPD_Tujuan_Awal</th>
+                                        <th>SKPD_Tujuan_Akhir</th>
+                                        <th>Rekomendasi_Bappeda_Mitra_OPD</th>
+                                        <th>Koefisien</th>
+                                        <th>Anggaran</th>
+                                        <th>Kategori_Usulan</th>
+                                        <th>Rekomendasi_Kelurahan_Desa</th>
+                                        <th>Koefisien</th>
+                                        <th>Anggaran</th>
+                                        <th>Rekomendasi_Kecamatan</th>
+                                        <th>Koefisien</th>
+                                        <th>Anggaran</th>
+                                        <th>Rekomendasi_SKPD</th>
+                                        <th>Koefisien</th>
+                                        <th>Anggaran</th>
+                                        <th>Rekomendasi_Bappeda</th>
+                                        <th>Koefisien</th>
+                                        <th>Anggaran</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($collection as $item)
+                                        <tr>
+                                            <td>{{ $item->No }}</td>
+                                            <td>{{ $item->Tgl_Usul }}</td>
+                                            <td>{{ $item->Profil }}</td>
+                                            <td>{{ $item->Pengusul }}</td>
+                                            <td>{{ $item->Urusan }}</td>
+                                            <td>{{ $item->Usulan }}</td>
+                                            <td>{{ $item->Permasalahan }}</td>
+                                            <td>{{ $item->Alamat }}</td>
+                                            <td>{{ $item->desa->nama }}</td>
+                                            <td>{{ $item->desa->kecamatan->nama }}</td>
+                                            <td>{{ $item->Usul_Ke }}</td>
+                                            <td>{{ $item->SKPD_Tujuan_Awal }}</td>
+                                            <td>{{ $item->SKPD_Tujuan_Akhir }}</td>
+                                            <td>{{ $item->Rekomendasi_Bappeda_Mitra_OPD }}</td>
+                                            <td>{{ $item->Koefisien }}</td>
+                                            <td>{{ $item->Anggaran }}</td>
+                                            <td>{{ $item->Kategori_Usulan }}</td>
+                                            <td>{{ $item->Rekomendasi_Kelurahan_Desa }}</td>
+                                            <td>{{ $item->Koefisien_1 }}</td>
+                                            <td>{{ $item->Anggaran_1 }}</td>
+                                            <td>{{ $item->Rekomendasi_Kecamatan }}</td>
+                                            <td>{{ $item->Koefisien_2 }}</td>
+                                            <td>{{ $item->Anggaran_2 }}</td>
+                                            <td>{{ $item->Rekomendasi_SKPD }}</td>
+                                            <td>{{ $item->Koefisien_3 }}</td>
+                                            <td>{{ $item->Anggaran_3 }}</td>
+                                            <td>{{ $item->Rekomendasi_Bappeda }}</td>
+                                            <td>{{ $item->Koefisien_4 }}</td>
+                                            <td>{{ $item->Anggaran_4 }}</td>
+                                            <td>{{ $item->Status }}</td>
+                                            <td>Action</td>
+                                        </tr>
+                                    @endforeach
+                                    {{-- <tfoot>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Tanggal Usul</th>
+                                            <th>Pengusul</th>
+                                            <th>Profil</th>
+                                            <th>Urusan</th>
+                                            <th>Usulan</th>
+                                            <th>Permasalahan</th>
+                                            <th>Alamat</th>
+                                            <th>Desa</th>
+                                            <th>Kecamatan</th>
+                                            <th>Usul_Ke</th>
+                                            <th>SKPD_Tujuan_Awal</th>
+                                            <th>SKPD_Tujuan_Akhir</th>
+                                            <th>Rekomendasi_Bappeda_Mitra_OPD</th>
+                                            <th>Koefisien</th>
+                                            <th>Anggaran</th>
+                                            <th>Kategori_Usulan</th>
+                                            <th>Rekomendasi_Kelurahan_Desa</th>
+                                            <th>Koefisien</th>
+                                            <th>Anggaran</th>
+                                            <th>Rekomendasi_Kecamatan</th>
+                                            <th>Koefisien</th>
+                                            <th>Anggaran</th>
+                                            <th>Rekomendasi_SKPD</th>
+                                            <th>Koefisien</th>
+                                            <th>Anggaran</th>
+                                            <th>Rekomendasi_Bappeda</th>
+                                            <th>Koefisien</th>
+                                            <th>Anggaran</th>
+                                            <th>Status</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </tfoot> --}}
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                </div>
+                <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
 @endsection
