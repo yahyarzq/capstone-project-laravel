1be65cf5c7d0b025fb2ea923ebdc8d180f25bba2
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-16 19:46:47 +0700

    Make table no longer wrap text

--- resources/views/dashboard/history_usulan.blade.php
+++ resources/views/dashboard/history_usulan.blade.php
@@ -11,7 +11,7 @@
                   </div>
                   <!-- /.card-header -->
                   <div class="card-body">
-                      <table id="example1" class="table table-bordered table-striped">
+                      <table id="example1" class="table table-bordered table-striped text-nowrap">
                         <thead>
                             <tr>
                                 <th>No</th>
