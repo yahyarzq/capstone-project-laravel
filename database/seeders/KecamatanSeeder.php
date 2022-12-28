b324d446858b27fdb7d27de81959f195cc8e6e90
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-28 12:38:58 +0700

    Delete "created_at" & "updated_at"

--- database/seeders/KecamatanSeeder.php
+++ database/seeders/KecamatanSeeder.php
@@ -18,201 +18,135 @@ class KecamatanSeeder extends Seeder
         $kecamatans = [
             [
                 "id" => 3507010,
-                "nama" => "DONOMULYO",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "DONOMULYO"
             ],
             [
                 "id" => 3507020,
-                "nama" => "KALIPARE",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "KALIPARE"
             ],
             [
                 "id" => 3507030,
-                "nama" => "PAGAK",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PAGAK"
             ],
             [
                 "id" => 3507040,
-                "nama" => "BANTUR",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "BANTUR"
             ],
             [
                 "id" => 3507050,
-                "nama" => "GEDANGAN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "GEDANGAN"
             ],
             [
                 "id" => 3507060,
-                "nama" => "SUMBERMANJING",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "SUMBERMANJING"
             ],
             [
                 "id" => 3507070,
-                "nama" => "DAMPIT",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "DAMPIT"
             ],
             [
                 "id" => 3507080,
-                "nama" => "TIRTO YUDO",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "TIRTOYUDO"
             ],
             [
                 "id" => 3507090,
-                "nama" => "AMPELGADING",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "AMPELGADING"
             ],
             [
                 "id" => 3507100,
-                "nama" => "PONCOKUSUMO",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PONCOKUSUMO"
             ],
             [
                 "id" => 3507110,
-                "nama" => "WAJAK",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "WAJAK"
             ],
             [
                 "id" => 3507120,
-                "nama" => "TUREN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "TUREN"
             ],
             [
                 "id" => 3507130,
-                "nama" => "BULULAWANG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "BULULAWANG"
             ],
             [
                 "id" => 3507140,
-                "nama" => "GONDANGLEGI",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "GONDANGLEGI"
             ],
             [
                 "id" => 3507150,
-                "nama" => "PAGELARAN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PAGELARAN"
             ],
             [
                 "id" => 3507160,
-                "nama" => "KEPANJEN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "KEPANJEN"
             ],
             [
                 "id" => 3507170,
-                "nama" => "SUMBER PUCUNG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "SUMBER PUCUNG"
             ],
             [
                 "id" => 3507180,
-                "nama" => "KROMENGAN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "KROMENGAN"
             ],
             [
                 "id" => 3507190,
-                "nama" => "NGAJUM",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "NGAJUM"
             ],
             [
                 "id" => 3507200,
-                "nama" => "WONOSARI",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "WONOSARI"
             ],
             [
                 "id" => 3507210,
-                "nama" => "WAGIR",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "WAGIR"
             ],
             [
                 "id" => 3507220,
-                "nama" => "PAKISAJI",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PAKISAJI"
             ],
             [
                 "id" => 3507230,
-                "nama" => "TAJINAN",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "TAJINAN"
             ],
             [
                 "id" => 3507240,
-                "nama" => "TUMPANG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "TUMPANG"
             ],
             [
                 "id" => 3507250,
-                "nama" => "PAKIS",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PAKIS"
             ],
             [
                 "id" => 3507260,
-                "nama" => "JABUNG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "JABUNG"
             ],
             [
                 "id" => 3507270,
-                "nama" => "LAWANG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "LAWANG"
             ],
             [
                 "id" => 3507280,
-                "nama" => "SINGOSARI",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "SINGOSARI"
             ],
             [
                 "id" => 3507290,
-                "nama" => "KARANGPLOSO",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "KARANGPLOSO"
             ],
             [
                 "id" => 3507300,
-                "nama" => "DAU",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "DAU"
             ],
             [
                 "id" => 3507310,
-                "nama" => "PUJON",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "PUJON"
             ],
             [
                 "id" => 3507320,
-                "nama" => "NGANTANG",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "NGANTANG"
             ],
             [
                 "id" => 3507330,
-                "nama" => "KASEMBON",
-                "created_at" => NULL,
-                "updated_at" => NULL,
+                "nama" => "KASEMBON"
             ]];
             foreach ($kecamatans as $kecamatan ) {
                 Kecamatan::create($kecamatan);
