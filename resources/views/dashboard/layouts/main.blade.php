0d5df19e5504ecb8bda443e55f2baa9cb99418eb
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2022-12-29 16:59:18 +0700

    Change date format to dd-mm-yyyy

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -197,7 +197,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     },
                     {
                         data: 'Tgl_Usul',
-                        name: 'Tgl_Usul'
+                        name: 'Tgl_Usul',
+                        render: function (data) { 
+                            return moment(data,'YYYY-MM-DD').format('DD-MM-YYYY'); 
+                        }
                     },
                     {
                         data: 'Pengusul',
