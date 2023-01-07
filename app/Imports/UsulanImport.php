72c5f96c2e427747834e4cce2d41e9f938287be2
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2023-01-07 09:25:58 +0700

    fix import model rules. set import to use batch inserts and chunk reading.

--- app/Imports/UsulanImport.php
+++ app/Imports/UsulanImport.php
@@ -3,9 +3,15 @@
 namespace App\Imports;
 
 use App\Models\Usulan;
+use Carbon\Carbon;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class UsulanImport implements ToModel
+
+class UsulanImport implements ToModel, WithStartRow, WithValidation, WithBatchInserts, WithChunkReading
 {
     /**
     * @param array $row
@@ -16,7 +22,7 @@ class UsulanImport implements ToModel
     {
         return new Usulan([
             'No' => $row[0],
-            'Tgl_Usul' => date('Y-m-d', strtotime($row[1])),
+            'Tgl_Usul' => Carbon::createFromFormat('d-m-Y',$row[1])->toDateString(),
             'Pengusul' => $row[2],
             'Profil' => $row[3],
             'Urusan' => $row[4],
@@ -24,7 +30,7 @@ class UsulanImport implements ToModel
             'Permasalahan' => $row[6],
             'Alamat' => $row[7],
             'Kecamatan' => $row[8],
-            'Kelurahan' => $row[9],
+            'Desa' => $row[9],
             'Usul_Ke' => $row[10],
             'SKPD_Tujuan_Awal' => $row[11],
             'SKPD_Tujuan_Akhir' => $row[12],
@@ -36,19 +42,74 @@ class UsulanImport implements ToModel
             'Rekomendasi_Kelurahan_Desa' => $row[18],
             'Rekomendasi_Kecamatan' => $row[19],
             'Koefisien_2' => $row[20],
-            'Anggaran_1' => $row[21],
+            'Anggaran_2' => $row[21],
             'Rekomendasi_SKPD' => $row[22],
             'Koefisien_3' => $row[23],
-            'Anggaran_2' => $row[24],
+            'Anggaran_3' => $row[24],
             'Rekomendasi_Bappeda' => $row[25],
             'Koefisien_4' => $row[26],
-            'Anggaran_3' => $row[27],
+            'Anggaran_4' => $row[27],
             'Status' => $row[28],
         ]);
     }
 
+    public function rules(): array
+    {
+        return [
+            '0' => 'required',
+            '1' => 'required|date_format:d-m-Y',
+            '2' => 'required',
+            '3' => 'required',
+            '4' => 'required',
+            '5' => 'required',
+            '6' => 'required',
+            '7' => 'required',
+            '8' => 'required',
+            '9' => 'required',
+            '10' => 'nullable',
+            '11' => 'required',
+            '12' => 'nullable',
+            '13' => 'nullable',
+            '14' => 'nullable',
+            '15' => 'nullable',
+            '16' => 'nullable',
+            '17' => 'nullable',
+            '18' => 'nullable',
+            '19' => 'nullable',
+            '20' => 'nullable',
+            '21' => 'nullable',
+            '22' => 'nullable',
+            '23' => 'nullable',
+            '24' => 'nullable',
+            '25' => 'nullable',
+            '26' => 'nullable',
+            '27' => 'nullable',
+            '28' => 'nullable',
+        ];
+    }
+
+    public function prepareForValidation($data, $index){
+
+        try {
+            $data[1] = \PhpOffice\PhpSpreadsheet\Shared\Date::excelToDateTimeObject($data[1])->format('d-m-Y') ?? Carbon::createFromFormat('d-m-Y',$data[1])->toDateString();
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+        return $data;
+    }
+
+    public function startRow(): int
+    {
+        return 2;
+    }
+
     public function chunkSize(): int
     {
-        return 1000;
+        return 50;
+    }
+
+    public function batchSize(): int
+    {
+        return 50;
     }
 }
