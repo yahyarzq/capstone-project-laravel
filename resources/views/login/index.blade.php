2c1084dc003ab1b1502d75fbe2e31c0c2896e113
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2023-01-10 21:56:19 +0700

    add wave background

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -13,61 +13,65 @@
   <link rel="stylesheet" href="../../plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../../dist/css/adminlte.min.css">
-</head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="#"><b>SIFULAN</b></a>
-  </div>
-  @if (session()->has('error'))
-  <div class="card-body">
-    <div class="alert alert-danger alert-dismissible">
-      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-      <h5><i class="icon fas fa-ban"></i>Log in Gagal!</h5>
-      Pastikan username dan password anda sudah benar
-    </div>
-  </div>
-  @endif
-  <!-- /.login-logo -->
-  <div class="card">
-    <div class="card-body login-card-body">
-      <p class="login-box-msg">Sign in untuk masuk</p>
+  <link rel="stylesheet" href="../../css/style.css">
 
-      <form action="/login" method="post">
-        @csrf
-        <div class="input-group mb-3">
-          <input type="username" name="username" class="form-control @error('username') is-invalid @enderror" placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
-          </div>
-          @error('username')
-              <div class="invalid-feedback">
-                {{ $message }}
+</head>
+<body class="hold-transition wave-pattern">
+  <div class="login-page" style="background-color: #28a745;">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="#" style="color: white;"><b>SIFULAN</b></a>
+      </div>
+      @if (session()->has('error'))
+      <div class="card-body">
+        <div class="alert alert-danger alert-dismissible">
+          <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+          <h5><i class="icon fas fa-ban"></i>Log in Gagal!</h5>
+          Pastikan username dan password anda sudah benar
+        </div>
+      </div>
+      @endif
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign in untuk masuk</p>
+    
+          <form action="/login" method="post">
+            @csrf
+            <div class="input-group mb-3">
+              <input type="username" name="username" class="form-control @error('username') is-invalid @enderror" placeholder="Username">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
               </div>
-          @enderror
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" name="password" class="form-control" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
+              @error('username')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+              @enderror
             </div>
-          </div>
+            <div class="input-group mb-3">
+              <input type="password" name="password" class="form-control" placeholder="Password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row justify-content-center">
+              <div class="col-10 ">
+                <button type="submit" class="btn btn-success btn-block" style="background-color: #28a745;">Sign In</button>
+              </div>
+              <!-- /.col -->
+            </div>
+          </form>
         </div>
-        <div class="row justify-content-center">
-          <div class="col-10 ">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
+        <!-- /.login-card-body -->
+      </div>
     </div>
-    <!-- /.login-card-body -->
+    <!-- /.login-box -->
   </div>
-</div>
-<!-- /.login-box -->
 
 <!-- jQuery -->
 <script src="../../plugins/jquery/jquery.min.js"></script>
