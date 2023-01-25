5afb7eeb4c9a0996fd7e1aa6cfe83f5faaae6836
yahyarzq <79823596+yahyarzq@users.noreply.github.com>
2023-01-25 21:28:39 +0700

    move the script to its respective page

--- resources/views/dashboard/history_usulan.blade.php
+++ resources/views/dashboard/history_usulan.blade.php
@@ -93,3 +93,223 @@
     </section>
     <!-- /.content -->
 @endsection
+
+
+@section("scripts")
+<script>
+    $(function() {
+        // $('#table_import').DataTable({
+        //     "paging": true,
+        //     "lengthChange": true,
+        //     "searching": false,
+        //     "ordering": true,
+        //     "info": true,
+        //     "autoWidth": false,
+        //     "responsive": false,
+        //     "scrollX": true,
+        //     "fixedHeader": true,
+        // }).columns.adjust();
+
+        $("#table-history").DataTable({
+            stateSave: true,
+            processing: true,
+            serverSide: true,
+            ajax: '/usulan/query/data',
+            columns: [{
+                    data: 'No',
+                    name: 'No'
+                },
+                {
+                    data: 'Tgl_Usul',
+                    name: 'Tgl_Usul',
+                    // render: function (data) { 
+                    //     return moment(data,'YYYY-MM-DD').format('DD-MM-YYYY'); 
+                    // }
+                },
+                {
+                    data: 'Pengusul',
+                    name: 'Pengusul'
+                },
+                {
+                    data: 'Profil',
+                    name: 'Profil'
+                },
+                {
+                    data: 'Urusan',
+                    name: 'Urusan'
+                },
+                {
+                    data: 'Usulan',
+                    name: 'Usulan'
+                },
+                {
+                    data: 'TipeUsulan',
+                    name: 'TipeUsulan'
+                },
+                {
+                    data: 'Permasalahan',
+                    name: 'Permasalahan'
+                },
+                {
+                    data: 'Alamat',
+                    name: 'Alamat'
+                },
+                {
+                    data: 'Desa',
+                    name: 'Desa'
+                },
+                {
+                    data: 'Kecamatan',
+                    name: 'Kecamatan'
+                },
+                {
+                    data: 'Usul_Ke',
+                    name: 'Usul_Ke'
+                },
+                {
+                    data: 'SKPD_Tujuan_Awal',
+                    name: 'SKPD_Tujuan_Awal'
+                },
+                {
+                    data: 'SKPD_Tujuan_Akhir',
+                    name: 'SKPD_Tujuan_Akhir'
+                },
+                {
+                    data: 'Rekomendasi_Bappeda_Mitra_OPD',
+                    name: 'Rekomendasi_Bappeda_Mitra_OPD'
+                },
+                {
+                    data: 'Koefisien',
+                    name: 'Koefisien'
+                },
+                {
+                    data: 'Anggaran',
+                    name: 'Anggaran'
+                },
+                {
+                    data: 'Kategori_Usulan',
+                    name: 'Kategori_Usulan'
+                },
+                {
+                    data: 'Koefisien_1',
+                    name: 'Koefisien_1'
+                },
+                {
+                    data: 'Rekomendasi_Kelurahan_Desa',
+                    name: 'Rekomendasi_Kelurahan_Desa'
+                },
+                // {
+                //     data: 'Koefisien_1',
+                //     name: 'Koefisien_1'
+                // },
+                // {
+                //     data: 'Anggaran_1',
+                //     name: 'Anggaran_1'
+                // },
+                {
+                    data: 'Rekomendasi_Kecamatan',
+                    name: 'Rekomendasi_Kecamatan'
+                },
+                {
+                    data: 'Koefisien_2',
+                    name: 'Koefisien_2'
+                },
+                {
+                    data: 'Anggaran_2',
+                    name: 'Anggaran_2'
+                },
+                {
+                    data: 'Rekomendasi_SKPD',
+                    name: 'Rekomendasi_SKPD'
+                },
+                {
+                    data: 'Koefisien_3',
+                    name: 'Koefisien_3'
+                },
+                {
+                    data: 'Anggaran_3',
+                    name: 'Anggaran_3'
+                },
+                {
+                    data: 'Rekomendasi_Bappeda',
+                    name: 'Rekomendasi_Bappeda'
+                },
+                {
+                    data: 'Koefisien_4',
+                    name: 'Koefisien_4'
+                },
+                {
+                    data: 'Anggaran_4',
+                    name: 'Anggaran_4'
+                },
+                {
+                    data: 'Status',
+                    name: 'Status',
+                    render: function(data,type,row){
+                        if (data === 'Usulan disetujui') {
+                            return `<span class="badge badge-success">${data}</span>`;
+                        } 
+                        else if (data === 'Usulan tidak disetujui') {
+                            return `<span class="badge badge-danger">${data}</span>`;
+                        } 
+                        else {
+                            return `<span class="badge badge-primary">${data}</span>`;
+                        }
+                    }
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    'render': function(data, type, row) {
+                        return `<a class="btn btn-primary btn-sm" id="btn-item-view"
+                                                data-id="${row.id}" data-toggle="modal"
+                                                data-attr="/usulan/${row.id}" data-target="#modal-view">
+                                                <i class="fas fa-folder">
+                                                </i>
+                                                View
+                                            </a>`
+                    },
+                    orderable: false, //Disable ordering on this column
+                    searchable: false //Disable search on this column
+                },
+
+            ],
+            initComplete: function() {},
+            "responsive": false,
+            "lengthChange": true,
+            "searching": true,
+            "autoWidth": false,
+            "scrollX": true,
+            "fixedHeader": false,
+
+
+        });
+        // $('#table-history').DataTable({
+        //     "paging": true,
+        //     "lengthChange": true,
+        //     "searching": true,
+        //     "ordering": true,
+        //     "info": false,
+        //     "autoWidth": false,
+        //     "responsive": false,
+        //     "scrollX": true,
+        // });
+    });
+
+
+    //     $(document).ready(function () {
+    //      var scrolledOnce;
+    //      $(window).on("scroll", function () {
+    //          let scrollTop = $(this).scrollTop();
+    //          if (scrollTop != 0) scrolledOnce = true;
+    //          if (scrollTop <= 0 && scrolledOnce) {
+    //              //top reached
+    //              table.columns.adjust();
+    //              //it seems fixedHead adjust not necessary
+    //              //table.fixedHeader.adjust();
+    //              scrolledOnce = false;
+    //          }
+    //      });
+    //  });
+</script>
+@endsection
--- resources/views/dashboard/usulan.blade.php
+++ resources/views/dashboard/usulan.blade.php
@@ -165,3 +165,280 @@
     </section>
     <!-- /.content -->
 @endsection
+
+
+@section("scripts")
+<script>
+    $(function() {
+        var datatable = $("#example1").DataTable({
+            stateSave: true,
+            processing: true,
+            serverSide: false,
+            ajax: '/usulan/data',
+            columns: [{
+                    data: 'No',
+                    name: 'No'
+                },
+                {
+                    data: 'Tgl_Usul',
+                    name: 'Tgl_Usul',
+                    // render: function (data) { 
+                    //     return moment(data,'YYYY-MM-DD').format('DD-MM-YYYY'); 
+                    // }
+                },
+                {
+                    data: 'Pengusul',
+                    name: 'Pengusul'
+                },
+                {
+                    data: 'Profil',
+                    name: 'Profil'
+                },
+                {
+                    data: 'Urusan',
+                    name: 'Urusan'
+                },
+                {
+                    data: 'Usulan',
+                    name: 'Usulan'
+                },
+                {
+                    data: 'TipeUsulan',
+                    name: 'TipeUsulan'
+                },
+                {
+                    data: 'Permasalahan',
+                    name: 'Permasalahan'
+                },
+                {
+                    data: 'Alamat',
+                    name: 'Alamat'
+                },
+                {
+                    data: 'Desa',
+                    name: 'Desa'
+                },
+                {
+                    data: 'Kecamatan',
+                    name: 'Kecamatan'
+                },
+                {
+                    data: 'Usul_Ke',
+                    name: 'Usul_Ke'
+                },
+                {
+                    data: 'SKPD_Tujuan_Awal',
+                    name: 'SKPD_Tujuan_Awal'
+                },
+                {
+                    data: 'SKPD_Tujuan_Akhir',
+                    name: 'SKPD_Tujuan_Akhir'
+                },
+                {
+                    data: 'Rekomendasi_Bappeda_Mitra_OPD',
+                    name: 'Rekomendasi_Bappeda_Mitra_OPD'
+                },
+                {
+                    data: 'Koefisien',
+                    name: 'Koefisien'
+                },
+                {
+                    data: 'Anggaran',
+                    name: 'Anggaran'
+                },
+                {
+                    data: 'Kategori_Usulan',
+                    name: 'Kategori_Usulan'
+                },
+                {
+                    data: 'Koefisien_1',
+                    name: 'Koefisien_1'
+                },
+                {
+                    data: 'Rekomendasi_Kelurahan_Desa',
+                    name: 'Rekomendasi_Kelurahan_Desa'
+                },
+                // {
+                //     data: 'Koefisien_1',
+                //     name: 'Koefisien_1'
+                // },
+                // {
+                //     data: 'Anggaran_1',
+                //     name: 'Anggaran_1'
+                // },
+                {
+                    data: 'Rekomendasi_Kecamatan',
+                    name: 'Rekomendasi_Kecamatan'
+                },
+                {
+                    data: 'Koefisien_2',
+                    name: 'Koefisien_2'
+                },
+                {
+                    data: 'Anggaran_2',
+                    name: 'Anggaran_2'
+                },
+                {
+                    data: 'Rekomendasi_SKPD',
+                    name: 'Rekomendasi_SKPD'
+                },
+                {
+                    data: 'Koefisien_3',
+                    name: 'Koefisien_3'
+                },
+                {
+                    data: 'Anggaran_3',
+                    name: 'Anggaran_3'
+                },
+                {
+                    data: 'Rekomendasi_Bappeda',
+                    name: 'Rekomendasi_Bappeda'
+                },
+                {
+                    data: 'Koefisien_4',
+                    name: 'Koefisien_4'
+                },
+                {
+                    data: 'Anggaran_4',
+                    name: 'Anggaran_4'
+                },
+                {
+                    data: 'Status',
+                    name: 'Status',
+                    render: function(data,type,row){
+                        if (data.toUpperCase() === 'USULAN DISETUJUI') {
+                            return `<span class="badge badge-success">${data}</span>`;
+                        } 
+                        else if (data.toUpperCase() === 'USULAN TIDAK DISETUJUI') {
+                            return `<span class="badge badge-danger">${data}</span>`;
+                        } 
+                        else {
+                            return `<span class="badge badge-primary">${data}</span>`;
+                        }
+                    }
+                },
+                {
+                    data: 'action',
+                    name: 'action',
+                    render: function(data, type, row) {
+                        return `<a class="btn btn-primary btn-sm" id="btn-item-view"
+                                                data-id="${row.id}" data-toggle="modal"
+                                                data-attr="/usulan/${row.id}" data-target="#modal-view">
+                                                <i class="fas fa-folder">
+                                                </i>
+                                                View
+                                            </a>
+                                <a class="btn btn-info btn-sm" id="btn-item-edit"
+                                                data-id="${row.id}" data-toggle="modal"
+                                                data-attr="/usulan/${row.id}" data-target="#modal-editS">
+                                                <i class="fas fa-pencil-alt">
+                                                </i>
+                                                Edit
+                                            </a>
+                                <a class="btn btn-danger btn-sm" id="btn-item-delete"
+                                                data-id="${row.id}" data-toggle="modal"
+                                                data-attr="/usulan/${row.id}" data-target="#modal-delete">
+                                                <i class="fas fa-trash">
+                                                </i>
+                                                Delete
+                                            </a>`;
+                    },
+                    orderable: false, //Disable ordering on this column
+                    searchable: false //Disable search on this column
+                }
+            ],
+            initComplete: function() {
+                $("#example1_wrapper > div:nth-child(1").after(
+                    '<div class="d-flex row ml-0 mb-1" id="filter_row"> </div>');
+                // var filter_row = $('<div class=" d-flex row ml-2" id="filter_row"> </div>')
+                //     .appendTo(
+                //         ' #example1_wrapper > div:nth-child(2)');
+                this.api()
+                    .columns([6, 1, 10, 9, 13])
+                    .every(function(d) {
+                        // var s1=$(`<div class="col-sm-2">
+                    //                 <div class="form-group" id="example1_wr_form_control">
+                    //                     <label>Select</label>
+                    //                     <div id="indse">
+                    //                         <select class="form-control" >
+                    //                             <option>option 1</option> 
+                    //                         </select>
+                    //                     </div>
+                    //                 </div>
+                    //             </div>`).appendTo('#fil_row');
+                        // var s1=`<div class="col-sm-2">
+                    //                 <div class="form-group" id="example1_wr_form_control">
+                    //                     <label>Select</label>
+                    //                         <select id="indse" class="form-control" >
+                    //                             <option>option 1</option> 
+                    //                         </select>
+                    //                 </div>
+                    //             </div>`;
+                        var column = this;
+                        var theadname = $('#example1 th').eq([d]).text();
+                        var select = $(
+                                '<select class="form-control "style="max-width: 140px; height: 38px;" ><option value="">' +
+                                theadname + '</option></select>'
+                            )
+                            .appendTo('#filter_row')
+                            .on('change', function() {
+                                var val = $.fn.dataTable.util.escapeRegex($(this).val());
+                                column.search(val ? '^' + val + '$' : '', true, false)
+                                    .draw();
+                            });
+
+                        column
+                            .data()
+                            .unique()
+                            .sort()
+                            .each(function(d, j) {
+                                select.append('<option value="' + d + '">' + d +
+                                    '</option>');
+                            });
+                    });
+                // columns.adjust();
+            },
+            // fixedColumns: {
+            //     left: 0, // Only for client side rendering
+            //     right: 0
+            //     // leftColumns: 1
+            // },
+            "responsive": false,
+            "lengthChange": true,
+            "searching": true,
+            "autoWidth": false,
+            "scrollX": true,
+            "fixedHeader": false,
+            // "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+        });
+
+        new $.fn.dataTable.Buttons( datatable, {
+            buttons: [
+                // {
+                //     text: 'Button 1',
+                //     action: function ( e, dt, node, conf ) {
+                //     console.log( 'Button 1 clicked on' );
+                //     }
+                // },
+                // {
+                //     text: 'Button 2',
+                //     action: function ( e, dt, node, conf ) {
+                //     console.log( 'Button 2 clicked on' );
+                //         }
+                // }
+                {
+                extend: 'excel',
+                title:'SIFULAN',
+                // messageTop: 'Data yang di export hanya data yang tampil',
+                exportOptions: {
+                        columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+                    }
+                }, "print", "colvis"
+            ]
+        });
+
+        datatable.buttons( 0, null ).container().prependTo('#example1_head');
+
+    });
+</script>
+@endsection
